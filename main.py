39cd4fa5218a87058e861e1e5c41a388e05fc118
szamir2197 <szamir2197@gmail.com>
2025-12-19 16:37:46 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+x = int(input("hafta kuni kirit: "))
+
+
+if x == 1:
+    print(f'{x} inchi kun bu dushanba')
+
+elif x == 2:
+    print(f'{x} inchi kun bu seshanba')
+
+elif x == 3:
+    print(f'{x} inchi kun bu chorshanba')
+
+elif x == 4:
+    print(f'{x} inchi kun bu payshanba')
+
+elif x == 5:
+    print(f'{x} inchi kun bu juma')
+
+elif x == 6:
+    print(f'{x} inchi kun bu shanba')
+
+elif x == 7:
+    print(f'{x} inchi kun bu yakshanba')
+
+else:
+    print(f'{x} kuni uchun hafta kuni yoq')
